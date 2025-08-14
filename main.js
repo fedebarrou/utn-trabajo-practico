b200fdeeb3cf8a94d6c8726d3a300a8a692f4756
fedeb-utn <fedeb90.ai@gmail.com>
2025-08-14 19:42:03 -0300

    Se utilizo $ para declarar elementos html

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const inputSend = document.querySelector("#textareaSend");
-const buttonSend = document.querySelector("#buttonSend");
-const inputSearch = document.querySelector("#inputSearch");
-const chatMessage = document.querySelector(".chat-messages");
+const $inputSend = document.querySelector("#textareaSend");
+const $buttonSend = document.querySelector("#buttonSend");
+const $inputSearch = document.querySelector("#inputSearch");
+const $chatMessage = document.querySelector(".chat-messages");
 
 
 
-buttonSend.addEventListener("click", () => {
+$buttonSend.addEventListener("click", () => {
 
 
-  let message = inputSend.value;
+  let message = $inputSend.value;
 
   createMenssage(`sent`, message);
   clearInput();
@@ -23,11 +23,12 @@ buttonSend.addEventListener("click", () => {
 
 
 
-inputSend.addEventListener("keydown", (event) => {
+$inputSend.addEventListener("keydown", (event) => {
 
-  let message = inputSend.value;
+  let message = $inputSend.value;
 
   if (event.key === "Enter") {
+    event.preventDefault();
     createMenssage("sent", message);
     clearInput();
 
@@ -50,19 +51,18 @@ function createMenssage(type, message) {
   const timeMessage = document.createElement("p");
   timeMessage.classList.add("timestamp");
   timeMessage.textContent = formatDate(new Date);
-
   divContainer.appendChild(textMessage);
   divContainer.appendChild(timeMessage);
 
-  chatMessage.appendChild(divContainer);
+  $chatMessage.appendChild(divContainer);
 
-  chatMessage.scrollTop = chatMessage.scrollHeight;
+  $chatMessage.scrollTop = $chatMessage.scrollHeight;
 
 }
 
 
 function clearInput() {
-  inputSend.value = "";
+  $inputSend.value = "";
 }
 
 
@@ -76,11 +76,9 @@ function formatDate(date) {
 
   // AM/PM
   const ampm = hours >= 12 ? "PM" : "AM";
-  // Si querés que siga mostrando de 0 a 24 y además AM/PM, no se convierte a 12h
-  // Si quisieras formato 12h, sería: hours = hours % 12 || 12;
 
   // Fecha y día
-  const opcionesFecha = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+  const opcionesFecha = { weekday: "short", year: "numeric", month: "short", day: "numeric" };
   const fechaTexto = isToday ? "Hoy" : date.toLocaleDateString("es-ES", opcionesFecha);
 
   return `${hours}:${minutes} ${ampm}, ${fechaTexto}`;
